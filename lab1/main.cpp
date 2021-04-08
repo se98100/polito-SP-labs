59618527572a64ede65eb2b2cadcc20e253be08f
se98100 <se98100@gmail.com>
2021-04-08 19:27:59 +0200

    Basic Message class

---
+++ lab1/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include "message.h"
+using namespace std;
+
+int main() {
+    Message m1(10);
+    Message m2(20);
+    Message m3;
+    cout << m1 << endl;
+    cout << m2 << endl;
+    cout << m3 << endl;
+}
